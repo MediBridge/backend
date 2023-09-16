a3c1cf33237ac30fc23a7a5667aa8ef684b13f41
Prakhar <prakharojha12@gmail.com>
2023-09-17 01:50:15 +0530

    refactored files

--- index.js
+++ index.js
@@ -1,86 +1,29 @@
 const express = require("express");
-const multer = require("multer");
-const pdf = require("pdf-parse");
 const cors = require("cors");
-const crypto = require("crypto");
-const { uploadData, getDataFromIpfs } = require("./utils/IPFS");
-const { encryptAES, decryptAES } = require("./utils/SerDe");
+
+//ROUTING IMPORTS
+const fileRoutes = require('./routes/FileHandling');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Configure multer for file upload
-const storage = multer.memoryStorage(); // Store file in memory
-const upload = multer({ storage: storage });
+
 app.use(
   cors({
     origin: "http://localhost:1234", // Replace with your React app's URL
   })
 );
 app.use(express.json());
-// Replace 'your-secret-key' with the actual key you want to use for encryption
 
-// Set up a route to handle file upload
-app.post("/fileUpload", upload.single("pdfFile"), async (req, res) => {
-  try {
-    if (!req.file) {
-      return res.status(400).send("No PDF file uploaded.");
-    }
-    console.log(req.body);
-    if (!req.body.key)
-      return res.status(400).send("Missing important parameters key");
-    console.log(req.body.key);
-    const secretKey = crypto
-      .createHash("sha256")
-      .update(String(req.body.key))
-      .digest("base64")
-      .substring(0, 32);
+// ALL FILE HANDLING ROUTES THAT INCLUDE File Encryption, File Decryption, Plain File Upload to IPFS.
 
-    const pdfBuffer = req.file.buffer;
-    const pdfData = await pdf(pdfBuffer);
+app.get('/api/healthcheck',(req,res)=>{
+  res.status(200).json({msg:"Working fine!"});
+})
+app.use('/api/file',fileRoutes);
 
-    // Process the PDF data as needed
-    const pdfText = pdfData.text;
-    // Encrypt the PDF text data using AES encryption
-    const encryptedPDFText = encryptAES(pdfText, secretKey);
 
-    // You can now process the encrypted text or perform any other tasks
-    console.log(JSON.stringify(encryptedPDFText));
-    const hashed = await uploadData(JSON.stringify(encryptedPDFText));
-    console.log(
-      `PDF file uploaded, processed, and encrypted successfully. ${hashed}`
-    );
-    res.status(200).json(hashed);
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("An error occurred during processing.");
-  }
-});
 
-// Set up a route to handle decryption
-app.post("/decrypt", async (req, res) => {
-  try {
-    const { url, key } = req.body;
-    console.log(req.body);
-    if (!url || !key)
-    return res.status(400).send("Missing important parameters url or key");
-
-    const secretKey = crypto
-      .createHash("sha256")
-      .update(String(key))
-      .digest("base64")
-      .substring(0, 32);
-    const data = await getDataFromIpfs(url);
-    const { iv, encryptedData } = data;
-
-    // Decrypt the encrypted data using AES decryption
-    const decryptedText = decryptAES(encryptedData, iv,secretKey);
-    res.status(200).json({ decryptedText });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("An error occurred during decryption.");
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
--- routes/FileHandling.js
+++ routes/FileHandling.js
@@ -1,1 +1,77 @@
-const router = reuire('express').router()
+const router = require('express').Router()
+const { uploadData, getDataFromIpfs } = require("../utils/IPFS");
+const { encryptAES, decryptAES } = require("../utils/SerDe");
+const pdf = require("pdf-parse");
+const crypto = require("crypto");
+const multer = require("multer");
+
+
+// Configure multer for file upload
+const storage = multer.memoryStorage(); // Store file in memory
+const upload = multer({ storage: storage });
+
+
+// THIS ROUTE ACCEPTS A FILE AND THEN RETURNS THE IPFS FILE AFTER HASHING IT
+router.post("/fileUploadUsingEncryption", upload.single("pdfFile"), async (req, res) => {
+    try {
+      if (!req.file) {
+        return res.status(400).send("No PDF file uploaded.");
+      }
+      console.log(req.body);
+      if (!req.body.key)
+        return res.status(400).send("Missing important parameters key");
+      console.log(req.body.key);
+      const secretKey = crypto
+        .createHash("sha256")
+        .update(String(req.body.key))
+        .digest("base64")
+        .substring(0, 32);
+  
+      const pdfBuffer = req.file.buffer;
+      const pdfData = await pdf(pdfBuffer);
+  
+      // Process the PDF data as needed
+      const pdfText = pdfData.text;
+      // Encrypt the PDF text data using AES encryption
+      const encryptedPDFText = encryptAES(pdfText, secretKey);
+  
+      // You can now process the encrypted text or perform any other tasks
+      console.log(JSON.stringify(encryptedPDFText));
+      const hashed = await uploadData(JSON.stringify(encryptedPDFText));
+      console.log(
+        `PDF file uploaded, processed, and encrypted successfully. ${hashed}`
+      );
+      res.status(200).json(hashed);
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).send("An error occurred during processing.");
+    }
+  });
+
+
+  router.post("/decryptFile", async (req, res) => {
+    try {
+      const { url, key } = req.body;
+      console.log(req.body);
+      if (!url || !key)
+      return res.status(400).send("Missing important parameters url or key");
+  
+      const secretKey = crypto
+        .createHash("sha256")
+        .update(String(key))
+        .digest("base64")
+        .substring(0, 32);
+      const data = await getDataFromIpfs(url);
+      const { iv, encryptedData } = data;
+  
+      // Decrypt the encrypted data using AES decryption
+      const decryptedText = decryptAES(encryptedData, iv,secretKey);
+      res.status(200).json({ decryptedText });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).send("An error occurred during decryption.");
+    }
+  });
+
+
+ module.exports = router
